bd9014cc02da30a464bdf9b8cced004d45ceed71
Jesse Herrera <jessalexherr@gmail.com>
2025-11-10 15:26:28 -0500

    5.475 speed up on gradescope

--- src/serial.c
+++ src/serial.c
@@ -1,13 +1,14 @@
+
 #include <dirent.h>
 #include <pthread.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <zlib.h>   // allowed library to compress the files
+#include <zlib.h>   //allowed library to compress the files
 #define BUFFER_SIZE 1048576 // 1MB
-#define MAX_FILE_SIZE (2 * 1048576) // 2MB
+
+#include <zlib.h>
 #define MAX_THREADS 19 // We should keep threads under 20 for this project */
 
 /*Group #: 25
@@ -15,7 +16,7 @@ Group Members (with NetIDs):
 Victoria Field, victoriafield
 Armani Romay, armaniromay
 Jesse Herrera, herrera42
-Jawaad Ramcharitar, jawaadramcharitar
+Jawaad Ramcharitar, jawaadramcharitar (test)
 Project Description: We are creating a program that will compress a directory of text files
 into a single zipped package with all text files in lexicographical order.
 We are going to use multiple threads to compress the files in parallel. The goal for this projectis to
@@ -39,6 +40,7 @@ typedef struct {
 	compressed_result_t *results;
 } worker_context_t;
 
+
 //Function to check if our file ends with .txt:
 static int ends_with_txt(const char *name) {
 	size_t len = strlen(name);
@@ -52,7 +54,7 @@ static int cmp(const void *a, const void *b) {
 	return strcmp(*(char * const *) a, *(char * const *) b);
 }
 
-//This function builds the full path of the file
+//This function builds the full path of our file
 static void build_full_path(const char *directory_name, const char *file_name, char *buffer, size_t buffer_size) {
 	size_t dir_len = strlen(directory_name);
 	int needs_slash = (dir_len > 0 && directory_name[dir_len - 1] == '/') ? 0 : 1;
@@ -63,82 +65,61 @@ static void build_full_path(const char *directory_name, const char *file_name, c
 		snprintf(buffer, buffer_size, "%s%s", directory_name, file_name);
 }
 
-// Worker thread: pull next file index, compress it entirely into memory,
-// and store the result in the pre-allocated results array at the same index.
-void *compress_worker(void *arg) {
+
+//compress worker here, use mutex locks for critical sections....
+static void *worker_thread(void *arg) {
 	worker_context_t *ctx = (worker_context_t *) arg;
-
 	for(;;) {
-		int file_index;
-
-		// critical section: atomically fetch next file index from shared counter
+		int my_index;
+		// get next index to process
 		pthread_mutex_lock(&ctx->lock);
 		if(ctx->next_index >= ctx->nfiles) {
-			// no more files to process, exit the thread
 			pthread_mutex_unlock(&ctx->lock);
 			break;
 		}
-		file_index = ctx->next_index;
-		ctx->next_index++;
+		my_index = ctx->next_index++;
 		pthread_mutex_unlock(&ctx->lock);
 
 		// build full path
-		char full_path[1024];
-		build_full_path(ctx->directory_name, ctx->files[file_index], full_path, sizeof(full_path));
+		char full_path[4096];
+		build_full_path(ctx->directory_name, ctx->files[my_index], full_path, sizeof(full_path));
 
-		// open file and get size
-		FILE *f_in = fopen(full_path, "rb");
+		// read file
+		FILE *f_in = fopen(full_path, "r");
 		assert(f_in != NULL);
-		
-		// get file size using stat (faster than fseek/ftell)
-		// struct stat st;
-		// int stat_ret = fstat(fileno(f_in), &st);
-		// assert(stat_ret == 0);
-		// size_t file_size = (size_t) st.st_size;
-		
-		// allocate buffer and read entire file in one operation
-		unsigned char *input_buffer = (unsigned char *) malloc(MAX_FILE_SIZE);
-		assert(input_buffer != NULL);
-		// if(file_size > 0) {
-		// 	size_t read_count = fread(input_buffer, 1, MAX_FILE_SIZE, f_in);
-		// 	assert(read_count == file_size);
-		// }
-		size_t nbytes = fread(input_buffer, 1, MAX_FILE_SIZE, f_in);
+		unsigned char *buffer_in = (unsigned char *) malloc(BUFFER_SIZE);
+		assert(buffer_in != NULL);
+		int nbytes = (int) fread(buffer_in, sizeof(unsigned char), BUFFER_SIZE, f_in);
 		fclose(f_in);
 
-		// allocate output buffer with sufficient capacity
-		uLongf bound = compressBound((uLong) nbytes);
-		unsigned char *output_buffer = (unsigned char *) malloc((size_t) bound);
-		assert(output_buffer != NULL || bound == 0);
-
 		// compress
+		unsigned char *buffer_out = (unsigned char *) malloc(BUFFER_SIZE);
+		assert(buffer_out != NULL);
 		z_stream strm;
-		memset(&strm, 0, sizeof(strm));
 		int ret = deflateInit(&strm, 9);
 		assert(ret == Z_OK);
-
-		strm.next_in = input_buffer;
 		strm.avail_in = (uInt) nbytes;
-		strm.next_out = output_buffer;
-		strm.avail_out = (uInt) bound;
+		strm.next_in = buffer_in;
+		strm.avail_out = BUFFER_SIZE;
+		strm.next_out = buffer_out;
 
 		ret = deflate(&strm, Z_FINISH);
 		assert(ret == Z_STREAM_END);
+		int nbytes_zipped = (int) (BUFFER_SIZE - strm.avail_out);
 		deflateEnd(&strm);
 
-		int compressed_size = (int)((size_t)bound - (size_t)strm.avail_out);
+		free(buffer_in);
 
 		// store result
-		ctx->results[file_index].data = output_buffer;
-		ctx->results[file_index].compressed_size = compressed_size;
-		ctx->results[file_index].original_size = (int) nbytes;
-
-		// free input
-		free(input_buffer);
+		ctx->results[my_index].data = buffer_out;
+		ctx->results[my_index].compressed_size = nbytes_zipped;
+		ctx->results[my_index].original_size = nbytes;
 	}
-
-	pthread_exit(NULL);
+	return NULL;
 }
+
+
+
 
 //Modidied function compress_directory for multiple threads:
 //function starts here:
@@ -174,57 +155,54 @@ void compress_directory(char *directory_name) {
 	}
 	closedir(d);
 	qsort(files, nfiles, sizeof(char *), cmp);
-	// create a single zipped package with all text files in lexicographical order
-	int total_in = 0, total_out = 0;
+	// prepare results and worker context
+	compressed_result_t *results = NULL;
+	if(nfiles > 0) {
+		results = (compressed_result_t *) calloc((size_t) nfiles, sizeof(compressed_result_t));
+		assert(results != NULL);
+	}
 
-	// initialize worker context
 	worker_context_t ctx;
 	ctx.files = files;
 	ctx.nfiles = nfiles;
 	ctx.directory_name = directory_name;
 	ctx.next_index = 0;
-	ctx.results = (compressed_result_t *) calloc((size_t) nfiles, sizeof(compressed_result_t));
-	assert(ctx.results != NULL || nfiles == 0);
+	ctx.results = results;
 	pthread_mutex_init(&ctx.lock, NULL);
 
-	// spin up at most MAX_THREADS threads
+	// spawn threads (up to MAX_THREADS). keep total threads <= 20 (main + workers)
 	int num_threads = nfiles < MAX_THREADS ? nfiles : MAX_THREADS;
 	pthread_t threads[MAX_THREADS];
-	for(int t = 0; t < num_threads; t++) {
-		int ret = pthread_create(&threads[t], NULL, compress_worker, &ctx);
-		assert(ret == 0);
+	for(int t=0; t<num_threads; t++) {
+		int rc = pthread_create(&threads[t], NULL, worker_thread, &ctx);
+		assert(rc == 0);
 	}
-	for(int t = 0; t < num_threads; t++) {
-		int ret = pthread_join(threads[t], NULL);
-		assert(ret == 0);
+	for(int t=0; t<num_threads; t++) {
+		pthread_join(threads[t], NULL);
 	}
+	pthread_mutex_destroy(&ctx.lock);
 
-	// write results in lex order to ensure correct output layout
-	FILE *f_out = fopen("text.tzip", "wb");
+	// write output in lexicographical order
+	int total_in = 0, total_out = 0;
+	FILE *f_out = fopen("text.tzip", "w");
 	assert(f_out != NULL);
-	
-	// use larger buffer for file output to reduce system calls
-	setvbuf(f_out, NULL, _IOFBF, BUFFER_SIZE);
-	
-	for(int i = 0; i < nfiles; i++) {
-		int nbytes_zipped = ctx.results[i].compressed_size;
+	for(int i=0; i < nfiles; i++) {
+		int nbytes_zipped = results[i].compressed_size;
 		fwrite(&nbytes_zipped, sizeof(int), 1, f_out);
-		if(nbytes_zipped > 0) {
-			fwrite(ctx.results[i].data, 1, (size_t) nbytes_zipped, f_out);
-		}
-		total_in += ctx.results[i].original_size;
+		fwrite(results[i].data, sizeof(unsigned char), (size_t) nbytes_zipped, f_out);
+		total_in += results[i].original_size;
 		total_out += nbytes_zipped;
-		free(ctx.results[i].data);
+		free(results[i].data);
 	}
 	fclose(f_out);
+	if(nfiles > 0) free(results);
 
-	pthread_mutex_destroy(&ctx.lock);
-	free(ctx.results);
-
-	printf("Compression rate: %.2lf%%\n", total_in == 0 ? 0.0 : 100.0*(total_in-total_out)/total_in);
+	printf("Compression rate: %.2lf%%\n", 100.0*(total_in-total_out)/(total_in == 0 ? 1 : total_in));
 
 	// release list of files
-	for(int i = 0; i < nfiles; i++)
+	for(int i=0; i < nfiles; i++)
 		free(files[i]);
 	free(files);
+
+	// do not modify the main function after this point!
 }
